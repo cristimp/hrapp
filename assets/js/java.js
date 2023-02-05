d1a39692513fb8060ebf2d5baa284b8c83561e7e
cristimp <124532364+cristimp@users.noreply.github.com>
2023-02-05 23:41:16 +0200

    Add files via upload

---
+++ assets/js/java.js
@@ -0,0 +1,19 @@
+
+// Dropdown search filter
+
+function filter() {
+    var keyword = document.getElementById("search").value;
+    var fleet = document.getElementById("select");
+    for (var i = 0; i < fleet.length; i++) {
+        var txt = fleet.options[i].text;
+        if (txt.substring(0, keyword.length).toLowerCase() !== keyword.toLowerCase() && keyword.trim() !== "") {
+            fleet.options[i].style.display = 'none';
+        } else {
+            fleet.options[i].style.display = 'list-item';
+        }
+    }
+}
+
+$(document).ready(function() {
+    $('.js-example-basic-multiple').select2();
+});
